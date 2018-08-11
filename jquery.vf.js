c4a9104e61d7cfd633a509c41130878399c81348
Malal91 <controlmalal@gmail.com>
2018-08-11 22:19:49 +0000

    add function

--- jquery.vf.js
+++ jquery.vf.js
@@ -1,30 +1,28 @@
 (function () {
-    $.fn.formValid = function (options) {
+    $.fn.vf = function (options) {
         var settings = $.extend({
             excludes: null,
         }, options);
 
-        var form_parent = $("#" + $(this).attr('id')).parents("form");
-        var inputs = $(form_parent).find('input').not(':button, :submit, :reset, :hidden');
-        var selects = $(form_parent).find('select');
+        var _parents = $(this).parents("form");
+        var _inputs = $(_parents).find('input').not(':button, :submit, :reset, :hidden');
+        var selects = $(_parents).find('select');
 
         $(this).click(function (e) {
-            $.each(inputs, function (key, element) {
-                var r = find($(this).attr('id'))
+            $.each(_inputs, function () {
+                var _r = find($(this).attr('name'))
 
-                if (r != true) {
-                    var type_input = $(this).attr('type')
-
+                if (_r != true) {
                     if ($(this).val() == '') {
                         e.preventDefault()
                         $(this).attr('style', 'border: 1px solid red')
                     } else {
-                        if (type_input == 'email') {
-                            if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test($('#' + $(this).attr('id')).val())) {
+                        if ($(this).attr('type') == 'email') {
+                            if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test($(this).val())) {
                                 $(this).attr('style', '')
                             } else {
                                 e.preventDefault();
-                                $('#' + $(this).attr('id')).attr('style', 'border: 1px solid red')
+                                $(this).attr('style', 'border: 1px solid red')
                             }
                         } else {
                             $(this).attr('style', '')
@@ -33,8 +31,8 @@
                 }
             })
 
-            $.each(selects, function (key, prop) {
-                var r = find($(this).attr('id'))
+            $.each(selects, function () {
+                var r = find($(this).attr('name'))
                 if (r != true) {
                     if ($(this).val() == '') {
                         e.preventDefault()
@@ -46,56 +44,53 @@
             })
         })
 
-        $('form input').focus(function () {
+        $('form input').not(':button, :submit, :reset, :hidden').focus(function () {
             $(this).attr('style', '')
         })
 
-        $('form input').blur(function () {
+        $('form input').not(':button, :submit, :reset, :hidden').blur(function () {
             if ($(this).attr('type') == 'email') {
                 if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test($(this).val())) {
                     $(this).attr('style', '')
                 } else {
-                    var r = find($(this).attr('id'))
-                    if (r != true) {
+                    var _r = find($(this).attr('id'))
+                    if (_r != true) {
                         $(this).attr('style', 'border: 1px solid red')
                     }
                 }
             } else {
                 if ($(this).val() != '') {
                     $(this).attr('style', '')
-                } else {
-                    var r = find($(this).attr('id'))
-                    if (r != true) {
+                }else {
+                    var _r = find($(this).attr('name'))
+                    if (_r != true) {
                         $(this).attr('style', 'border: 1px solid red')
                     }
-
                 }
             }
-
         })
 
         $('form select').change(function () {
             if ($(this).val() != '') {
                 $(this).attr('style', '');
             } else {
-                var r = find($(this).attr('id'))
-                if (r != true) {
+                var _r = find($(this).attr('name'))
+                if (_r != true) {
                     $(this).attr('style', 'border: 1px solid red');
                 }
             }
         })
 
         function find(el) {
-            var val = false
+            var _val = false
             if (settings.excludes != null) {
                 for (var i = 0; i < settings.excludes.length; i++) {
                     if (el == settings.excludes[i]) {
-                        val = true
+                        _val = true
                     }
                 }
             }
-            return val
+            return _val
         }
     }
-
 }(jQuery));
